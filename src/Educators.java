bd9f326010ec388f1b565f62d053183fc62f58be
Xin1306 <149458454+Xin1306@users.noreply.github.com>
2024-04-12 14:57:03 +0800

    Add files via upload

---
+++ src/Educators.java
@@ -0,0 +1,32 @@
+public class Educators {
+    protected int numOfQuiz;
+    protected int numOfEvent;
+    public Educators(){
+        this.numOfEvent = 0;
+        this.numOfQuiz = 0;
+    }
+
+    public int getNumOfQuiz() {
+        return numOfQuiz;
+    }
+
+    public void setNumOfQuiz(int numOfQuiz) {
+        this.numOfQuiz = numOfQuiz;
+    }
+
+    public int getNumOfEvent() {
+        return numOfEvent;
+    }
+
+    public void setNumOfEvent(int numOfEvent) {
+        this.numOfEvent = numOfEvent;
+    }
+
+    @Override
+    public String toString() {
+        return "Educators{" +
+                "numOfQuiz=" + numOfQuiz +
+                ", numOfEvent=" + numOfEvent +
+                '}';
+    }
+}
---
+++ src/Quiz.java
@@ -0,0 +1,65 @@
+public class Quiz {
+    protected String quizTitle;
+    protected String description;
+    protected String theme;
+    protected String content;
+    private boolean completed;
+    public Quiz(String quizTitle,String description,String theme,String content){
+        this.quizTitle= quizTitle;
+        this.description= description;
+        this.theme= theme;
+        this.content= content;
+        this.completed = false;
+    }
+
+    public String getQuizTitle() {
+        return quizTitle;
+    }
+
+    public void setQuizTitle(String quizTitle) {
+        this.quizTitle = quizTitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "quizTitle='" + quizTitle + '\'' +
+                ", description='" + description + '\'' +
+                ", theme='" + theme + '\'' +
+                ", content='" + content + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
