d031025c84c05fd0aa59815a0a2a295c8c386d2d
Eterran <146816991+Eterran@users.noreply.github.com>
2024-04-29 14:20:30 +0800

    Fixed typo

--- src/Quiz.java
+++ src/Quiz.java
@@ -66,7 +66,7 @@ public class Quiz {
                 ", theme='" + theme + '\'' +
                 ", content='" + content + '\'' +
                 ", completed=" + completed +
-                ", numOfQuizCreate=" + numOfQuizCreate +
+                ", numOfQuizCreate=" + numOfQuizCreated +
                 '}';
     }
 }
