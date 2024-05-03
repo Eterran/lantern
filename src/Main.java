77fd59d083ba6aad185c428a3cb522b3d0150db1
Xin1306 <149458454+Xin1306@users.noreply.github.com>
2024-05-03 12:55:40 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,45 +16,4 @@ public class Main {
         
 
     }
-
-    
-    private static void accessCreateQuizPage(List<Quiz> quizzes){
-        System.out.println("Enter quiz title: ");
-        String quizTitle = sc.nextLine();
-        System.out.println("Enter quiz description: ");
-        String description = sc.nextLine();
-        System.out.println("Enter quiz theme (Science/ Technology/ Engineering/ Mathematics): ");
-        String theme = sc.nextLine();
-        System.out.println("Enter quiz content: ");
-        String content = sc.nextLine();
-
-        Quiz newQuiz = new Quiz(quizTitle, description, theme, content);
-        quizzes.add(newQuiz);
-        numOfQuizCreated++;
-        System.out.println("Quiz added successfully!");
-    }
-    public void viewQuizzes(List<Quiz> quizzes){
-        System.out.println("Please select the theme to filter(comma to separated):");
-        String[] filters = sc.nextLine().split(",");
-        boolean filtered = false;
-        for(Quiz quiz: quizzes){
-            for(String filter: filters){
-                if(quiz.getTheme().equalsIgnoreCase(filter)){
-                    System.out.println(quiz.quizTitle);
-                    filtered = true;
-                    break;
-                }
-            }
-        }
-        if(!filtered){
-            System.out.println("No quizzes found for the selected themes.");
-        }
-    }
-
-    public void attemptQuiz(Quiz quiz){
-        System.out.println("Attempting Quiz: "+ quiz.quizTitle);
-        completedQuizzes.add(quiz);
-        points += 2;
-        System.out.println("Quiz completed! You have been awarded 2 marks.");
-    }
 }
