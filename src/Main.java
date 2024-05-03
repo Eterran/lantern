65e1652f006ab2d6c155ae4c30e445ff4a98cd98
Xin1306 <149458454+Xin1306@users.noreply.github.com>
2024-05-03 12:53:32 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -17,20 +17,6 @@ public class Main {
 
     }
 
-    public static void viewEvent(List<Event> eventList){
-        if(eventList.isEmpty()){
-            System.out.println("No events available.");
-            return;
-        }
-        //Display live event
-        System.out.println("Live Events : "+ eventList.get(0));
-        //Display closest 3 upcoming events
-        System.out.println("Closest 3 Upcoming Events : ");
-        for(int i =0; i<3; i++) {
-            System.out.println((i + 1) + "." + eventList.get(i));
-        }
-
-    }
     public void register(Event event){
         System.out.println("Select an event: ");
         String choice = sc.nextLine();
