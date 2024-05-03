d70da574a7ced1059090327ba7e5934245aeb8ce
Xin1306 <149458454+Xin1306@users.noreply.github.com>
2024-05-03 12:55:15 +0800

    Update Event.java

--- src/Event.java
+++ src/Event.java
@@ -71,6 +71,55 @@ public class Event {
         }
 
     }
+    public void register(Event event){
+        System.out.println("Select an event: ");
+        String choice = sc.nextLine();
+        //Check if the event is already registered
+        for(Event registeredEvent : registeredEvents){
+            if(choice.equalsIgnoreCase(registeredEvent.getEventTitle())){
+                System.out.println("You are already registered for this events.");
+                return;
+            }
+        }
+        //Check for date clashes with already registered events
+        for(Event registeredEvent : registeredEvents){
+            if(event.getDate().equals(registeredEvent.getDate()));{
+                System.out.println("You are already registered for an event on the same day. Please choose another event ^^");
+                return;
+            }
+        }
+
+        //If no clashes
+        Event chosenEvent = null;
+        for(Event e : eventList){
+            if(e.getEventTitle().equalsIgnoreCase(choice)){
+                chosenEvent = e;
+                break;
+            }
+        }
+        registeredEvents.add(chosenEvent);
+        points+=5;
+        System.out.println("You have successfully registered for the event: "+ chosenEvent.getEventTitle());
+        System.out.println("You have been awarded 5 marks!");
+    }
+
+    private static void accessCreateEventPage(List<Event> eventList){
+        System.out.println("Enter event title: ");
+        String eventTitle = sc.nextLine();
+        System.out.println("Enter event description: ");
+        String descrip = sc.nextLine();
+        System.out.println("Enter event venue: ");
+        String venue = sc.nextLine();
+        System.out.println("Enter event date: ");
+        String date = sc.nextLine();
+        System.out.println("Enter event time: ");
+        String time = sc.nextLine();
+
+        Event newEvent = new Event(eventTitle, descrip, venue, date, time);
+        eventList.add(newEvent);
+        numOfEventCreated++;
+        System.out.println("Event added successfully!");
+    }
 
     @Override
     public String toString() {
